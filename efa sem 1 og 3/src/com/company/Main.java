f6b54fc04c1e224a7dc0770965a4ac8ed60be310
Kristian-j-k <89967661+Kristian-j-k@users.noreply.github.com>
2021-09-06 10:34:26 +0200

    update

---
+++ efa sem 1 og 3/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner Hej = new Scanner(System.in);
+        System.out.println("Enter username");
+
+        String userName = Hej.nextLine();
+        System.out.println("Username is "+userName);
+
+    }
+}
